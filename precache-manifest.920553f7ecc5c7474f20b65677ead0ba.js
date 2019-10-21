1f338ce5b52401fa893579d0c8732f772e855b07
browniu <niuniusoga@qq.com>
2019-10-21 11:01:48 +0800

    Updates

---
+++ precache-manifest.920553f7ecc5c7474f20b65677ead0ba.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d2d2a07744e2918737e9884c5a09b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6f93d4f1a6390b97025",
+    "url": "./static/css/main.951dd03a.chunk.css"
+  },
+  {
+    "revision": "565537a6b7e387386cea",
+    "url": "./static/js/2.fb9470d5.chunk.js"
+  },
+  {
+    "revision": "e6f93d4f1a6390b97025",
+    "url": "./static/js/main.0fabdd6d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "2947f3184e353ca4056d96e3499047e5",
+    "url": "./static/media/plane.2947f318.jpeg"
+  },
+  {
+    "revision": "cd91047a725261fcced6d084e29f5019",
+    "url": "./static/media/pose.cd91047a.jpg"
+  },
+  {
+    "revision": "0ada55ed484933cbd9c0aad1e087b2e9",
+    "url": "./static/media/pose10.0ada55ed.jpg"
+  },
+  {
+    "revision": "f2493662782667944ce831c05d145477",
+    "url": "./static/media/pose11.f2493662.jpeg"
+  },
+  {
+    "revision": "61a0dff7e0015eb6f5f6d953607769cc",
+    "url": "./static/media/pose12.61a0dff7.jpeg"
+  },
+  {
+    "revision": "41a1e0b2d22388968a42356e35aeec63",
+    "url": "./static/media/pose4.41a1e0b2.jpg"
+  },
+  {
+    "revision": "f364329536511270a2b7b58fd0174301",
+    "url": "./static/media/pose6.f3643295.jpeg"
+  },
+  {
+    "revision": "2c022f2d59ebf7cd9a750e23689d88f6",
+    "url": "./static/media/pose8.2c022f2d.jpg"
+  },
+  {
+    "revision": "db68f59c8163dd2132b851f3e53e4770",
+    "url": "./static/media/pose9.db68f59c.jpg"
+  }
+]);
